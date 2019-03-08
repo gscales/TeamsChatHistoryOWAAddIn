1b7a7bc9436e548b8c31a005973ca6814fce6e87
Glen Scales <gscales@gsx.com>
2019-03-08 15:50:54 +1100

    checkin

---
+++ MessageRead.js
@@ -0,0 +1,111 @@
+﻿(function () {
+    "use strict";
+
+    var messageBanner;
+
+    // The Office initialize function must be run each time a new page is loaded.
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            var element = document.querySelector('.ms-MessageBanner');
+            messageBanner = new fabric.MessageBanner(element);
+            messageBanner.hideBanner();
+            if(Office.context.mailbox.item.sender.emailAddress == "noreply@email.teams.microsoft.com"){
+                resolveName(Office.context.mailbox.item.sender.displayName.replace(" in Teams",""));
+            }else{
+                getRestAccessToken(Office.context.mailbox.item.sender.emailAddress);
+            }
+
+        });
+
+    };
+
+    function getRestAccessToken(EmailAddress){
+        Office.context.mailbox.getCallbackTokenAsync({ isRest: true }, function (result) {
+            if (result.status === "succeeded") {
+                var accessToken = result.value;
+                getChatMessages(accessToken,EmailAddress);                
+            } else {
+                // Handle the error
+            }
+        });
+    }
+    function getChatMessages(accessToken,emailAddress) {
+        var filterString = "SingleValueExtendedProperties/Any(ep: ep/PropertyId eq 'String 0x001a' and ep/Value eq 'IPM.SkypeTeams.Message') and from/emailAddress/address eq '" + emailAddress  + "'";
+        var GetURL = "https://outlook.office.com/api/v2.0/me/MailFolders/AllItems/messages?$Top=100&$Select=ReceivedDateTime,bodyPreview,webLink&$filter=" + filterString;
+        $.ajax({
+            type: "Get",
+            contentType: "application/json; charset=utf-8",
+            url: GetURL,
+            dataType: 'json',
+            headers: { 'Authorization': 'Bearer ' + accessToken }
+        }).done(function (item) {
+            DisplayMessages(item.value);
+        }).fail(function (error) {
+            $('#mTchatTable').append("Error getting Messages " + error);
+        });
+    }
+
+    function resolveName(NameToLookup){
+        var request = GetResolveNameRequest(NameToLookup);
+        var EmailAddress = "";        
+        Office.context.mailbox.makeEwsRequestAsync(request, function (asyncResult) {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(asyncResult.value, "text/xml");
+            var values = doc.getElementsByTagName("t:EmailAddress");
+            if(values.length != 0){
+                EmailAddress = values[0].textContent;
+                getRestAccessToken(EmailAddress);
+            } 
+            
+
+        });
+
+    }
+   
+    function GetResolveNameRequest(NameToLookup) {
+        var results =    
+
+        '<?xml version="1.0" encoding="utf-8"?>' +
+        '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
+        '  <soap:Header>' +
+        '    <t:RequestServerVersion Version="Exchange2013" />' +
+        '  </soap:Header>' +
+        '  <soap:Body>' +
+        '    <m:ResolveNames ReturnFullContactData="true" SearchScope="ActiveDirectory">' +
+        '      <m:UnresolvedEntry>' + NameToLookup + '</m:UnresolvedEntry>' +
+        '    </m:ResolveNames>' +
+        '  </soap:Body>' +
+        '</soap:Envelope>'
+         return results;
+    }
+    
+    function DisplayMessages(Messages) {
+        try {
+            var html = "<div class=\"ms-Table-row\">";
+            html = html + "<span class=\"ms-Table-cell\" >ReceivedDateTime</span>";
+            html = html + "<span class=\"ms-Table-cell\">BodyPreview</span>";
+            html = html + "</div>";
+            var i;
+            for (i = (Messages.length-1); i >= 0 ; i--) { 
+                var rcvDate = Date.parse(Messages[i].ReceivedDateTime);
+                html = html + "<div class=\"ms-Table-row\">";
+                html = html +"<span class=\"ms-Table-cell\">" + rcvDate.toString('dd-MMM-yy HH:mm') + "</span>";
+                html = html +"<span id=\"Subject\" class=\"ms-Table-cell\">";
+                html = html + Messages[i].BodyPreview + " <a target='_blank' href='" + Messages[i].WebLink + "'> Link</a></span ></div >";
+            }
+
+            $('#mTchatTable').append(html);
+        } catch (error) {
+            $('#mTchatTable').html("Error displaying table " + error);
+        }
+    }
+
+
+    // Helper function for displaying notifications
+    function showNotification(header, content) {
+        $("#notificationHeader").text(header);
+        $("#notificationBody").text(content);
+        messageBanner.showBanner();
+        messageBanner.toggleExpansion();
+    }
+})();
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# TeamsChatHistoryOWAAddIn
-Microsoft Teams Chat History Outlook/OWA Addin
+# Microsoft Teams Chat History Outlook/OWA Addin #
+
+Being somebody who is transitioning across from Skype for Business to Teams one of things I missed the most (and found the most frustrating) is the lack of the ability in Outlook and OWA to view the conversation history from Online meetings and private chats in Microsoft Teams. This is especially frustrating when you have an external meeting and your sent an IM that contains some vital information for what you need to do. This information is tracked in your mailbox for compliance reasons in the Teams Chat folder but this folder is hidden so it not accessible to the clients and must be extracted by other means eg. Most people seem to point to doing a compliance search if you need this data [https://docs.microsoft.com/en-us/microsoftteams/security-compliance-overview](https://docs.microsoft.com/en-us/microsoftteams/security-compliance-overview) .
+
+Given that the information is in my mailbox and there shouldn't be any privacy concern arounds access it I looked at few ways of getting access to these TeamsChat messages in OWA and Outlook the first way was using a SearchFolder. This did kind of work but because of a few quirks that the Hidden folder caused was only usable when using Outlook in online mode (which isn't very usable). The next thing I did was look a using an Addin which worked surprising well and was relatively easy to implement. Here is what it looks likes in action all you need to do is find a Message from the user you want to view the chat message from and then a query will be executed to find the Chat messages from that user using the Outlook REST endpoint eg
+
+![](https://1.bp.blogspot.com/-j_fyvriDXUQ/XIHgQL1X3cI/AAAAAAAACS0/nJKkqlPsjdIJpNohzk6p9Mi_DmmZwU3LACLcBGAs/s1600/tcHist1.JPG)
+
+
+That constructs a query that look like the following to Outlook REST endpoint
+
+    https://outlook.office.com/api/v2.0/me/MailFolders/AllItems/messages?$Top=100&amp;$Select=ReceivedDateTime,bodyPreview,weblink&amp;$filter=SingleValueExtendedProperties/Any(ep: ep/PropertyId eq 'String 0x001a' and ep/Value eq 'IPM.SkypeTeams.Message') and from/emailAddress/address eq 'e5tmp5@domain.com' 
+
+To break this down a bit first this gets the first 100 messages from the AllItems Search Folder
+
+ https://outlook.office.com/api/v2.0/me/MailFolders/AllItems/messages?$Top=100
+Next this selects the properties we are going to use the table to display, I used body preview because for IM's that generally don't have subjects so getting the body preview text is generally good enough to shown the whole message. But if the message is longer the link is provided which will open up in a new OWA windows using the weblink property which contains a full path to open the Item. One useful things about opening the message this way is you can then click replay and continue a message from IM in email with the body context from the IM (I know this will really erk some Teams people but i think it pretty cool and has proven useful for me).
+
+    $Select=ReceivedDateTime,bodyPreview,weblink
+
+Next this is the filter that is applied so it only returns the IM chat message (or those messages that have an ItemClass of IPM.SkypeTeams.Message and are from the sender assoicated with the Message you activate the Addin on.
+
+ `$filter=SingleValueExtendedProperties/Any(ep: ep/PropertyId eq 'String 0x001a' and ep/Value eq 'IPM.SkypeTeams.Message') and from/emailAddress/address eq 'e5tmp5@datarumble.com'`
+
+One thing I did find after using this for a while is that it didn't work when I got a notification from teams like the following
+
+
+
+Because the above search came from noreply@email.teams.microsoft.com it couldn't be used in the above query. Looking at the notification message unfortunately there wasn't any other properties that did contain the email address but the full displayname of the user was used in the email's displayName so as a quick workaround for these I made use of EWS's resolvenames operation to resolve the displayName to an email address and then I could use the Addin even on the notification messages to see the private chat message that was sent to me within OWA without needing to open the Teams app (which if you have multiple tennants can be a real pain). So this one turned into a real productiviy enhancer for me.
+
+Want to give it a try yourself ?
+
+I've hosted the files on my GitHub pages so its easy to test (if you like it clone it and host it somewhere else). But all you need to do is add it as a custom addin (if you allowed to) using the 
+URL-
+
+  https://gscales.github.io/TeamsChatHistory/TeamsChatHistory.xml
+
+![](https://1.bp.blogspot.com/-fV2Wxo0Cr7Q/XIHvqow9SVI/AAAAAAAACTM/Vo-Pc3Q74AgZ-_KNh_Nl9UhZmBbouJS1wCLcBGAs/s1600/addin3.JPG)
+
+
+
